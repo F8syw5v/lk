6ec7379af4342446c30d4c83a408937c64d73ba4
房顶居士 <108336535+lala123963@users.noreply.github.com>
2022-11-15 11:08:26 +0800

    Update telecom_live_lotter3.py

--- dianxin/telecom_live_lotter3.py
+++ dianxin/telecom_live_lotter3.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # -- coding: utf-8 --
 # -------------------------------
-
+# @Author : github@limoruirui https://github.com/limoruirui
+# @Time : 2022/11/11 10:42
 # -------------------------------
 # cron "*/30 10-20 * * *"
-# const $ = new Env('某营业厅直播抽奖3');
+# const $ = new Env('某营业厅直播抽奖');
 """
 1. 脚本仅供学习交流使用, 请在下载后24h内删除
 2. 环境变量说明:
@@ -15,11 +16,14 @@
 from random import randint
 from re import findall
 from time import mktime, strptime
-from requests import post, get
+from requests import post, get, packages
+packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ":HIGH:!DH:!aNULL"
 from datetime import datetime
 from base64 import b64encode
+from asyncio import get_event_loop, wait, sleep, run
 
-from tool import timestamp, get_environ
+from tool import timestamp, get_environ, print_now
+from send_msg import push
 from china_telecom import ChinaTelecom
 
 class TelecomLotter:
@@ -70,23 +74,86 @@ class TelecomLotter:
             return None
         except:
             return None
-    def lotter(self, liveId, period):
-        active_code = self.get_action_id(liveId)
-        if active_code is None:
-            print(f"此直播间无抽奖活动, 退出")
-            return
-        url = "https://xbk.189.cn/xbkapi/active/v2/lottery/do"
+    def get_action_id_other(self, liveId):
+        def encrypt_phone():
+            result = ""
+            for i in self.phone:
+                result += chr(ord(i) + 2)
+            return result
+        url = "https://wapmkt.189.cn:8301/query/directSeedingInfo"
         body = {
-            "active_code": active_code,
-            "liveId": liveId,
-            "period": period
+            "headerInfos": {
+                "code": "directSeedingInfo",
+                "timestamp": datetime.now().__format__("%Y%m%d%H%M%S"),
+                "broadAccount": "",
+                "broadToken": "",
+                "clientType": "#9.6.1#channel128#samsung SM-G9860#",
+                "shopId": "20002",
+                "source": "110003",
+                "sourcePassword": "Sid98s",
+                "token": self.token,
+                "userLoginName": self.phone
+            },
+            "content": {
+                "attach": "test",
+                "fieldData": {
+                    "liveId": liveId,
+                    "account": encrypt_phone()
+                }
+            }
         }
         headers = {
             "User-Agent": self.ua,
             "authorization": self.authorization
         }
-        data = post(url, headers=headers, json=body).json()
-        print(data)
+        data = post(url, headers=headers, json=body).json()["responseData"]["data"]
+        try:
+            if data["buoyLink"] is None:
+                return None
+            active_code = findall(r"active_code\u003d(.*?)\u0026", data["buoyLink"])[0]
+            return active_code
+        except:
+            return None
+    async def lotter(self, liveId, period):
+        """
+        :param liveId: 直播间id
+        :param period: 某个参数 暂不明意义 查询直播间信息时会返回
+        :return:
+        """
+        print_now(f"当前执行的直播间id为{liveId}")
+        for i in range(8):
+            # active_code1 查询直播间购物车中的大转盘活动id
+            active_code1 = self.get_action_id(liveId)
+            # active_code2 查询直播间非购物车 而是右上角的大转盘活动id
+            active_code2 = self.get_action_id_other(liveId)
+            if active_code1 is not None or active_code2 is not None:
+                break
+            print(f"此直播间暂无抽奖活动, 等待90秒后再次查询 剩余查询次数{7 - i}")
+            await sleep(90)
+            continue
+        if active_code1 is None and active_code2 is None:
+            print("查询结束 本直播间暂无抽奖活动")
+            return
+        elif active_code1 is None or active_code2 is None:
+            active_code = active_code1 if active_code2 is None else active_code2
+            active_code_list = [active_code]
+        else:
+            active_code_list = [active_code1, active_code2]
+        for active_code in active_code_list:
+            url = "https://xbk.189.cn/xbkapi/active/v2/lottery/do"
+            body = {
+                "active_code": active_code,
+                "liveId": liveId,
+                "period": period
+            }
+            headers = {
+                "User-Agent": self.ua,
+                "authorization": self.authorization
+            }
+            data = post(url, headers=headers, json=body).json()
+            print(data)
+            if data["code"] == 0:
+                push("直播抽奖", f"获得了{data['data']['title']}")
 
 def main(phone, password):
     url = "https://xbk.189.cn/xbkapi/lteration/index/recommend/anchorRecommend?provinceCode=21"
@@ -97,9 +164,23 @@ def main(phone, password):
     }
     data = get(url, headers=headers).json()
     if data["code"] == 0:
+        liveListInfo = {}
         for liveInfo in data["data"]:
             if 1740 > timestamp(True) - int(mktime(strptime(liveInfo["start_time"], "%Y-%m-%d %H:%M:%S"))) > 0:
-                TelecomLotter(phone, password).lotter(liveInfo["liveId"], liveInfo["period"])
+                liveListInfo[liveInfo["liveId"]] = liveInfo["period"]
+        if len(liveListInfo) == 0:
+            print("查询结束 没有近期开播的直播间")
+        elif len(liveListInfo) == 1:
+            for liveId, period in liveListInfo.items():
+                run(TelecomLotter(phone, password).lotter(liveId, period))
+        elif len(liveListInfo) >= 2:
+            telecomLotter = TelecomLotter(phone, password)
+            all_task = [telecomLotter.lotter(liveId, period) for liveId, period in liveListInfo.items()]
+            loop = get_event_loop()
+            loop.run_until_complete(wait(all_task))
+            loop.close()
+    else:
+        print(f"查询直播间信息失败 接口返回: ------\n{data}")
 
 if __name__ == '__main__':
     phone = get_environ("TELECOM_PHONE3")
